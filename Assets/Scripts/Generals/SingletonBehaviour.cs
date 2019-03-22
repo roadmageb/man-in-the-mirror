fda84f058e68b9d25d36e0178f8b041749d332c6
15박보승 <pbs04054@gmail.com>
2019-03-23 00:44:56 +0900

    SingletonBehaviour 추가

---
+++ Assets/Scripts/Generals/SingletonBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T _inst = null;
+	public static T inst
+	{
+		get
+		{
+			if (_inst == null)
+			{
+				if (FindObjectsOfType<T>().Length > 1)
+					Debug.LogError("More than one");
+				else if (FindObjectOfType<T>() != null)
+					_inst = FindObjectOfType<T>();
+				else
+				{
+					GameObject go = new GameObject();
+					go.name = typeof(T).Name;
+					_inst = go.AddComponent<T>();
+				}
+			}
+			return _inst;
+		}
+	}
+
+	public void SetStatic()
+	{
+		if (_inst != null)
+			DontDestroyOnLoad(_inst.gameObject);
+	}
+}
