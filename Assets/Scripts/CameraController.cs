b9870243716061969f7619677091e74043b27233
18손재민 <jasson31@naver.com>
2019-03-22 21:09:34 +0900

    카메라 워크 wip

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.position += new Vector3(verticalInput + horizontalInput, 0, verticalInput - horizontalInput);
+    }
+}
