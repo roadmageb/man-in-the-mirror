20847b4bb5158d14577458b7d4f7b8f95d11963c
18신대성 <esc990720@korea.ac.kr>
2019-03-23 10:22:31 +0900

    캐릭터 idle, walk 애니메이션 추가

---
+++ Assets/Models/Anim_Player/AnimTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            anim.SetBool("isWalking", true);
+        else if (Input.GetMouseButtonDown(1))
+            anim.SetBool("isWalking", false);
+    }
+}
