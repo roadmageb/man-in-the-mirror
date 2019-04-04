7bfd07105ebde5cdc9976c5c7022723cade6dec4
박보승 <pbs04054@gmail.com>
2019-04-04 15:53:31 +0900

    Vector2Int 맨해튼 거리 extension 추가

---
+++ Assets/Scripts/Extensions/Vector2IntExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector2IntExtensions
+{
+    public static int ManhattanDistance(this Vector2Int a, Vector2Int b)
+    {
+        return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
+    }
+}
