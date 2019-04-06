fdb3878cc87a295599b870676c92d447a0371983
15박보승 <pbs04054@gmail.com>
2019-04-06 19:11:30 +0900

    마네킹 로직 변경

--- Assets/Scripts/Interactors/Mannequin.cs
+++ Assets/Scripts/Interactors/Mannequin.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class Mannequin : MonoBehaviour, IBulletInteractor
 {
+	[SerializeField]
+	private Mesh[] mannequinMesh = new Mesh[2];
+	private Color _color;
     public Color Color {
         get
         {
-            return GetComponent<MeshRenderer>().material.color;
+			return _color;
         }
         private set
         {
-            GetComponent<MeshRenderer>().material.color = value;
-        }
+			if (value == Color.black)
+			{
+				GetComponent<MeshFilter>().mesh = mannequinMesh[0];
+				//Change mesh to black mannequin
+			}
+			else if (value == Color.white)
+			{
+				GetComponent<MeshFilter>().mesh = mannequinMesh[1];
+				//Change mesh to white mannequin
+			}
+			else
+			{
+				Debug.LogWarning("Invalid color input");
+			}
+			_color = value;
+		}
     }
 
     public void Interact(Bullet bullet)
     {
         if (bullet is TruthBullet)
         {
-            GetComponent<MeshRenderer>().material.color = Color.white;
+           Color = Color.white;
         }
         if (bullet is FakeBullet)
         {
-            GetComponent<MeshRenderer>().material.color = Color.black;
+			Color = Color.black;
         }
     }
 
