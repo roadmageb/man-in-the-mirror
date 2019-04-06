158d28ea71d91f9db1a953fa50545c094cc881b3
18신대성 <esc990720@korea.ac.kr>
2019-04-06 21:26:42 +0900

    반사 매트리얼 및 대응코드 만들었음 ModelScene에서 플레이 누르고 씬창에서 카메라 움직여보면 됨

---
+++ Assets/Scripts/MirrorReflect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// code that moves Reflection Probe to proper position
+public class MirrorReflect : MonoBehaviour
+{
+    enum Direction { X, Z } // +X direction and +Z direction. Mirror on both side
+
+    [SerializeField]
+    private Direction direction;
+
+    public Transform mirror;
+    public Transform mainCam;
+
+    private float offset;
+    private Vector3 probePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCam = GameObject.Find("Camera").transform; // find main camera
+
+        if (direction == Direction.X)
+        {
+            offset = mirror.position.x - mainCam.position.x;
+
+            probePos.x = mirror.position.x + offset;
+            probePos.y = mainCam.position.y;
+            probePos.z = mainCam.position.z;
+        }
+        else if (direction == Direction.Z)
+        {
+            offset = mirror.position.z - mainCam.position.z;
+
+            probePos.x = mainCam.position.x;
+            probePos.y = mainCam.position.y;
+            probePos.z = mirror.position.z + offset;
+        }
+
+        transform.position = probePos;
+    }
+}
