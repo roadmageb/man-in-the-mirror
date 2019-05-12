15bcb18eb86c5c7961ca2cf283599fb2d081cf9a
18손재민 <jasson31@naver.com>
2019-05-12 16:11:35 +0900

    카메라 회전 일단 구현

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    Vector3 dragOrigin;
+    public float dragSpeed;
 
     void CameraMove()
     {
@@ -12,15 +14,40 @@ public class CameraController : MonoBehaviour
         transform.position += new Vector3(verticalInput + horizontalInput, 0, verticalInput - horizontalInput);
     }
 
+    void CameraDrag()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(1)) return;
+
+        float deg = Mathf.Atan2(transform.position.z, transform.position.x);
+        float dis = Vector3.Distance(Vector3.zero, transform.position - new Vector3(0, transform.position.y, 0));
+
+        float dif = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin).x * dragSpeed;
+
+        transform.position = new Vector3(Mathf.Cos(deg + dif) * dis, transform.position.y, Mathf.Sin(deg + dif) * dis);
+        transform.LookAt(new Vector3(0, 0, 0));
+        dragOrigin = Input.mousePosition;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
         CameraMove();
+        CameraDrag();
     }
+
 }
