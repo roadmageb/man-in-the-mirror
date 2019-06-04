9b28305f2f9710a1869b9d2c96681b67478a3b52
Daeseong Shin <esc990720@korea.ac.kr>
2019-06-04 22:39:01 +0900

    이번엔 진짜 제대로 움직임 AnimTest에서 참고해서 보시오.

--- Assets/Models/Animation/AnimTest.cs
+++ Assets/Models/Animation/AnimTest.cs
@@ -5,32 +5,44 @@ using UnityEngine;
 public class AnimTest : MonoBehaviour
 {
     Animator anim;
-    [Range(-1, 1)] public float xAngle = 0;
+    public Transform aimObject;
+    public Transform target;
+    Quaternion aimRotation;
+    bool isShooting = false;
 
     private void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        aimRotation = aimObject.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.W))
             anim.SetBool("isWalking", true);
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S))
             anim.SetBool("isWalking", false);
-        else if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            anim.SetTrigger("shoot");
-        }
-        else if (Input.GetKey(KeyCode.F))
-            anim.ResetTrigger("shoot");
-        else if (Input.GetKey(KeyCode.G))
             anim.SetBool("isShooting", true);
-        else if (Input.GetKey(KeyCode.H))
+            isShooting = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
             anim.SetBool("isShooting", false);
+            isShooting = false;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+            anim.SetTrigger("shoot");
 
-        anim.SetLayerWeight(1, Mathf.Abs(xAngle));
-        anim.SetFloat("xAngle", xAngle);
+        if (isShooting)
+        {
+            aimObject.LookAt(target, new Vector3(0,1,0));
+        }
+        else
+        {
+            aimObject.rotation = aimRotation;
+        }
     }
 }
