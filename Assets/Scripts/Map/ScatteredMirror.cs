e962a84efe9664f904436d3d96d93968f882049b
18신대성 <esc990720@korea.ac.kr>
2019-07-10 17:49:03 +0900

    거울 깨지면 4초뒤부터 서서히 사라지게 만듬

---
+++ Assets/Scripts/Map/ScatteredMirror.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScatteredMirror : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Smaller());
+    }
+    
+    IEnumerator Smaller()
+    {
+        yield return new WaitForSeconds(4f);
+        for (int i = 100; i > 0; i--)
+        {
+            Vector3 scale = new Vector3(i,i,i);
+            for (int j = 0; j < 100; j++) transform.GetChild(j).transform.localScale = scale;
+            yield return new WaitForSeconds(0.03f);
+        }
+        Destroy(gameObject);
+    }
+}
