a8977f0641977983f068dbdf953241023f19562b
Merseong <esc990720@korea.ac.kr>
2020-01-04 14:52:16 +0900

    Add ray to RayPole object

---
+++ Assets/Scripts/Interactors/LightPole.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPole : MonoBehaviour, IObject, IBulletInteractor
+{
+    private Transform shootPoint;
+    private LineRenderer rayRenderer; 
+
+    private bool isRayActive = false;
+    private float rayHeight;
+
+    private void Start()
+    {
+        shootPoint = transform.Find("RotateObject");
+        rayRenderer = GetComponent<LineRenderer>();
+
+        rayRenderer.SetPosition(0, shootPoint.position);
+        rayRenderer.SetPosition(1, shootPoint.position);
+        rayHeight = shootPoint.localPosition.y;
+
+        SetRayActive(true);
+    }
+
+    public void SetRayActive(bool isActive)
+    {
+        if (isRayActive != isActive)
+        {
+            isRayActive = isActive;
+            if (isActive)
+            { // turn on ray
+                rayRenderer.enabled = true;
+                ShootRay();
+            }
+            else
+            { // turn off ray
+                rayRenderer.enabled = false;
+            }
+        }
+    }
+
+    public void ShootRay()
+    {
+        // transform.forward 잘쓰면될듯
+        List<Vector3> points = new List<Vector3>();
+        Vector3 lastPoint = shootPoint.position;
+        Vector3 lastDirection = transform.forward;
+        int maxSize = 10; // MapManager.inst.currentMap.maxMapSize
+        RaycastHit hit;
+        bool isHit;
+
+        points.Add(lastPoint);
+        do
+        {
+            isHit = Physics.Raycast(lastPoint, lastDirection, out hit, maxSize);
+            if (isHit)
+            {
+                lastPoint = hit.transform.position;
+                lastPoint.y = rayHeight;
+                points.Add(lastPoint);
+
+                isHit = false; // for test
+            }
+            else
+            {
+                points.Add(lastPoint + lastDirection * maxSize);
+            }
+        } while (isHit && maxSize > Mathf.Max(lastPoint.x, lastPoint.z));
+        rayRenderer.SetPositions(points.ToArray());
+    }
+
+    public IEnumerator RotatePole()
+    {
+        SetRayActive(false);
+        yield return null;
+        SetRayActive(true);
+    }
+
+    #region IObject
+    public void Init(Floor floor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject GetObject()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector2Int GetPos()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    ObjType IObject.GetType()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float GetRadius()
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+
+    #region IBulletInteractor
+    public void Interact(Bullet bullet)
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+}
