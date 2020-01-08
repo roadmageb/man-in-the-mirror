d86ee943e6ece525c63ed1658c57ebcd72684797
jasson31 <jasson31@naver.com>
2020-01-08 18:08:06 +0900

    문제 푸는 기계 만들고싶음

---
+++ Assets/Scripts/SolveMachine/SolveMap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolveMap
+{
+    public int maxMapSize;
+    public Vector2Int maxBorder, minBorder;
+    public Dictionary<Vector2Int, bool> floorGrid;
+    public Dictionary<Vector2, WallType> wallGrid;
+    public Dictionary<Vector2, ObjType> objectGrid;
+
+    public List<KeyValuePair<Vector2Int, bool>> playerFloors;
+    public List<BulletCode> initialBullets;
+    public List<ClearCondition> clearConditions;
+
+    public SolveMap()
+    {
+        floorGrid = new Dictionary<Vector2Int, bool>();
+        wallGrid = new Dictionary<Vector2, WallType>();
+        objectGrid = new Dictionary<Vector2, ObjType>();
+        playerFloors = new List<KeyValuePair<Vector2Int, bool>>();
+        initialBullets = new List<BulletCode>();
+        clearConditions = new List<ClearCondition>();
+    }
+    public SolveMap(SolveMap svm)
+    {
+        floorGrid = new Dictionary<Vector2Int, bool>(svm.floorGrid);
+        wallGrid = new Dictionary<Vector2, WallType>(svm.wallGrid);
+        objectGrid = new Dictionary<Vector2, ObjType>(svm.objectGrid);
+        playerFloors = new List<KeyValuePair<Vector2Int, bool>>(svm.playerFloors);
+        initialBullets = new List<BulletCode>(svm.initialBullets);
+        clearConditions = new List<ClearCondition>(svm.clearConditions);
+    }
+
+}
---
+++ Assets/Scripts/SolveMachine/Solver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solver
+{
+    /// <summary>
+    /// 해의 집합
+    /// </summary>
+    public List<List<string>> solutions;
+
+    public List<List<string>> GetSolution(SolveMap map)
+    {
+        solutions = new List<List<string>>();
+        return solutions;
+    }
+    void Recursive(List<string> solution, SolveMap map)
+    {
+        if(CheckClear(map))
+        {
+            solutions.Add(solution);
+            return;
+        }
+        SolveMap originMap = new SolveMap(map);
+        //move
+        foreach(KeyValuePair<Vector2Int, bool> player in map.playerFloors)
+        {
+            if(player.Value)
+            {
+
+            }
+        }
+        //shoot
+
+        //getBox
+    }
+    bool CheckClear(SolveMap map)
+    {
+        return true;
+    }
+}
